4e67daa57eb10772c76d558b4a92955539f21fdb
m10ev <mtenev@outlook.com>
2026-01-08 09:12:13 +0200

    SSH Works

--- Forked/Services/EmailSender.cs
+++ Forked/Services/EmailSender.cs
@@ -7,6 +7,7 @@ namespace Forked.Services
 {
     public class EmailSender : IEmailSender
     {
+        //no -s
         private readonly IConfiguration _config;
         public EmailSender(IConfiguration config)
         {
