8499b1de4d08281a161ee55638b065340c7f61bd
m10ev <mtenev@outlook.com>
2026-01-12 21:56:25 +0200

    Added Recipe fields, added new table RecipeIngredients

--- Forked/Models/Domains/Recipe.cs
+++ Forked/Models/Domains/Recipe.cs
@@ -4,5 +4,15 @@ namespace Forked.Models.Domains
 {
     public class Recipe : BaseEntity
     {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string? ImagePath { get; set; }
+        public int? PreparationTimeInMinutes { get; set; }
+        public int? CookingTimeInMinutes { get; set; }
+        public int Servings { get; set; }
+        public string? AuthorId { get; set; }
+        public User? Author { get; set; }
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+        public ICollection<UserFavoriteRecipe> FavoritedByUsers { get; set; } = new List<UserFavoriteRecipe>();
     }
 }
---
+++ Forked/Models/Domains/RecipeIngredient.cs
@@ -0,0 +1,13 @@
+﻿namespace Forked.Models.Domains
+{
+    public class RecipeIngredient
+    {
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; } = null!;
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; } = null!;
+        public double Quantity { get; set; } = 0;
+        public string Unit { get; set; } = string.Empty;
+        public string Preparation { get; set; } = string.Empty;
+    }
+}
