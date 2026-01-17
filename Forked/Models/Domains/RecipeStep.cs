32249dbace1a426eaa5c8cce74965b7d1f9b1cc1
korayali <korayali8209@gmail.com>
2026-01-17 10:04:35 +0200

    Added RecipeStep, UserFavoriteRecipe

--- Forked/Models/Domains/RecipeStep.cs
+++ Forked/Models/Domains/RecipeStep.cs
@@ -2,5 +2,16 @@
 {
     public class RecipeStep
     {
+        public int Id { get; set; }
+
+        public int StepNumber { get; set; }
+
+        public string StepName { get; set; } = string.Empty;
+        public string Instruction { get; set; } = string.Empty;
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; } = null!;
+
+        public List<string> ImagePaths { get; set; } = new();
+
     }
 }
--- Forked/Models/Domains/UserFavoriteRecipe.cs
+++ Forked/Models/Domains/UserFavoriteRecipe.cs
@@ -2,5 +2,11 @@
 {
     public class UserFavoriteRecipe
     {
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; } = null!;
+        public DateTime FavoritedAt { get; set; } = DateTime.UtcNow;
     }
 }
