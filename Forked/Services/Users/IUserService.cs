c6f3e95b4b80f1b624037917cbea083750df4df3
korayali <korayali8209@gmail.com>
2026-02-14 13:35:04 +0200

    Changes in IUserService

--- Forked/Services/Users/IUserService.cs
+++ Forked/Services/Users/IUserService.cs
@@ -6,11 +6,11 @@ namespace Forked.Services.Users
     public interface IUserService
     {
         Task<User> CreateAsync(User user);
-        Task<UserDetailsViewModel?> GetUserDetailsAsync(string userId, string? currentUserId);
-        Task<UserFollowersListViewModel> GetFollowersAsync(string userId, int page, int pageSize); 
-        Task<UserFollowingListViewModel> GetFollowingAsync(string userId, int page, int pageSize);
-        Task<UserCardViewModel?> GetUserCardAsync(string userId);
-        Task DeleteAsync(string userId);
+        Task<UserDetailsViewModel?> GetUserDetailsAsync(string displayName, string? currentUserId);
+        Task<UserFollowersListViewModel> GetFollowersAsync(string displayName, int page, int pageSize); 
+        Task<UserFollowingListViewModel> GetFollowingAsync(string displayName, int page, int pageSize);
+        Task<UserCardViewModel?> GetUserCardAsync(string displayName);
+        Task DeleteAsync(string displayName);
         Task FollowAsync(string id, string displayName);
         Task UnfollowAsync(string id, string displayName);
     }
